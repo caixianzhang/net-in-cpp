3b22946dfcde8e73d80ad1fa3a276dc3a4621347
caixianzhang <2369173394@qq.com>
2018-10-17 23:34:02 +0800

    修正main函数，使用字符串类

--- main.cpp
+++ main.cpp
@@ -2,37 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string>
 #include "main.h"
 #include "net.h"
 
 #define NET "ens33"
-#define IP "224.21.21.70"
 #define PORT 2170
 #define MESS "ABCD"
+#define IP "224.21.21.70"
+
+using namespace std;
 
 int main(int argc, char **argv)
 {
-	char eth[40] = {0};
-	strncpy(eth, NET, strlen(NET));
-
-	char My_IP[40] = {0};
-	strncpy(My_IP, IP, strlen(IP));
-
-	char Mess[5] = {0};
-	strncpy(Mess, MESS, strlen(MESS));
+	string eth = NET;
+	string My_IP = IP;
 	
-	
+	Multicast_Recv  *client = new Multicast_Recv(My_IP, PORT, eth, 320*1024, 0);
+
 	char buff[5] = {0};
-	strncpy(buff, MESS, strlen(MESS));
-
-	Multicast_Send *server = new Multicast_Send(My_IP, PORT, eth, 8);
 	
 	while(1)
 	{
-		server->Send((unsigned char *)buff, 5);
+		client->Recv((unsigned char*)buff, 5);
 		printf("%s\n", buff);
-		sleep(1);
 	}
 	
 	return 0;
